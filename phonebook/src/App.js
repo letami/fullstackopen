02c17bda19a285622f8e9c198c0ee436e0796ef1
letami <Bien66hoa>
2023-08-15 16:54:09 +0200

    2.11 Added effect hooks to get data from json server

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,18 +1,22 @@
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
 import Numbers from './components/Numbers'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
+import axios from "axios";
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        {name: 'Arto Hellas', number: '040-123456', id: 1},
-        {name: 'Ada Lovelace', number: '39-44-5323523', id: 2},
-        {name: 'Dan Abramov', number: '12-43-234345', id: 3},
-        {name: 'Mary Poppendieck', number: '39-23-6423122', id: 4}
-    ])
+    const [persons, setPersons] = useState([])
     const [filter, setFilter] = useState('')
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
+
+    useEffect(() => {
+        axios
+            .get('http://localhost:3001/persons')
+            .then(response =>{
+                setPersons(response.data)
+            })
+    }, [])
 
     const addPerson = (event) => {
         event.preventDefault()
@@ -31,7 +35,8 @@ const App = () => {
             <h2>Phonebook</h2>
             <Filter handleChange={handleFilterChange}/>
             <h2>add a new person</h2>
-            <PersonForm addPerson={addPerson} handleNameChange={handleNameChange} handlePhoneChange={handlePhoneChange}/>
+            <PersonForm addPerson={addPerson} handleNameChange={handleNameChange}
+                        handlePhoneChange={handlePhoneChange}/>
             <Numbers persons={filteredPersons}/>
         </div>
     )
