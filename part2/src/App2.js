ab79cec28e74793b3b5a7107ee9eb72d324b5c65
letami <julian.tam.le@web.de>
2023-08-16 17:01:09 +0200

    refactored service and added errorhandling

--- part2/src/App2.js
+++ part2/src/App2.js
@@ -1,6 +1,6 @@
 import {useState, useEffect} from 'react'
-import axios from "axios";
 import Note from './components/Note'
+import noteService from './services/notes'
 
 const App = () => {
     const [notes, setNotes] = useState([])
@@ -8,12 +8,10 @@ const App = () => {
     const [showAll, setShowAll] = useState(true)
 
     useEffect(() => {
-        console.log('effect')
-        axios
-            .get('http://localhost:3001/notes')
-            .then(response => {
-                console.log('promise fulfilled')
-                setNotes(response.data)
+        noteService
+            .getAll()
+            .then(initialNotes => {
+                setNotes(initialNotes)
             })
     }, [])
 
@@ -24,24 +22,26 @@ const App = () => {
             important: Math.random() < 0.5
         }
 
-        axios
-            .post('http://localhost:3001/notes', noteObject)
-            .then(response => {
-                setNotes(notes.concat(response.data))
+        noteService
+            .create(noteObject)
+            .then(returnedNote => {
+                setNotes(notes.concat(returnedNote))
                 setNewNote('')
             })
-
     }
 
     const toggleImportanceOf = (id) => {
-        const url = `http://localhost:3001/notes/${id}`
         const note = notes.find(n => n.id === id)
         const changedNote = {...note, important: !note.important}
 
-        axios
-            .put(url, changedNote)
-            .then(response => {
-                setNotes(notes.map(n => n.id !== id ? n : response.data))
+        noteService
+            .update(id, changedNote)
+            .then(returnNote => {
+                setNotes(notes.map(note => note.id !== id ? note : returnNote))
+            })
+            .catch(error => {
+                alert(`the note ${note.content} was already deleted from server`)
+                setNotes(notes.filter(n => n.id !== id))
             })
     }
 
