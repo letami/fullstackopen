1f39f47ab61fdfaf8ae05e09b015728d2dda2601
letami <julian.tam.le@web.de>
2023-08-22 13:33:40 +0200

    getOne and delete functionality

--- part3-backend/index.js
+++ part3-backend/index.js
@@ -19,12 +19,30 @@ let notes = [
     }
 ]
 
-app.get('/', (request, response)=>  {
+app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
     response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const note = notes.find(note => note.id === id)
+
+    if (note) {
+        response.json(note)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+
+    response.status(204).end()
 })
 
 const PORT = 3001
