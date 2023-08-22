90ac58ac77ec6ef2a43cd804690af1c6550a2077
letami <julian.tam.le@web.de>
2023-08-22 16:54:40 +0200

    added morgan for logging

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
-const {request, response} = require("express");
+const morgan = require('morgan')
+
 const app = express()
 
 app.use(express.json())
+app.use(morgan('tiny'))
 
 let persons = [
     {
@@ -27,7 +29,7 @@ let persons = [
     }
 ]
 
-app.get('/', (request, response) => {
+app.get('/', function (request, response) {
     response.send('<h1>Welcome to the phonebook backend</h1>')
 })
 
