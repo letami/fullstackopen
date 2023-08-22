7c6035ed9eeeb589389ec3fa73f53b06def1c220
letami <julian.tam.le@web.de>
2023-08-22 17:01:12 +0200

    custom token and format for morgan

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -3,8 +3,12 @@ const morgan = require('morgan')
 
 const app = express()
 
+morgan.token('json', function displayJson (req) {
+    return JSON.stringify(req.body)
+})
+
 app.use(express.json())
-app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :json'))
 
 let persons = [
     {
